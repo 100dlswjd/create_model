723250445d442283c20ac390c798f6dee8bc99e7
100dlswjd <100dlswjd@naver.com>
2023-06-20 18:41:43 +0900

    메인 코드 작성

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Signal, Slot
+
+from ui.main_form import Ui_MainWindow
+from ui.class_tab_form import Ui_class_tab
+
+class ClassTab(QMainWindow, Ui_class_tab):
+    def __init__(self, class_name : str):
+        super().__init__()
+        self.setupUi(self)
+        self.class_name = class_name
+        self.label_class_name.setText(class_name)
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_add_class.clicked.connect(self.btn_add_class_tab_handler)
+        self.lineEdit_class_name.returnPressed.connect(self.btn_add_class_tab_handler)
+    
+    def btn_add_class_tab_handler(self):
+        if not self.lineEdit_class_name.text():
+            return False
+        name = self.lineEdit_class_name.text()
+        self.tabWidget.addTab(ClassTab(name), name)
+        self.lineEdit_class_name.setText("")
+        return True
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
