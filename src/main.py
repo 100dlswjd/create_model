eefbc85ff21e078b15ce3e656dc26bd6be987c50
100dlswjd <100dlswjd@naver.com>
2023-06-22 18:45:22 +0900

    쓰레드 관련해서 제작중.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
@@ -5,6 +6,14 @@ from PySide6.QtCore import Signal, Slot
 
 from ui.main_form import Ui_MainWindow
 from ui.class_tab_form import Ui_class_tab
+
+from threading import Thread
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 class ClassTab(QMainWindow, Ui_class_tab):
     def __init__(self, class_name : str):
@@ -17,24 +26,46 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.tab_list = list()
         self.pushButton_add_class.clicked.connect(self.btn_add_class_tab_handler)
         self.lineEdit_class_name.returnPressed.connect(self.btn_add_class_tab_handler)
         self.pushButton_delete_class.clicked.connect(self.btn_delete_class_tab_handler)
+        self.pushButton_make.clicked.connect(self.btn_make_handler)
     
+    @Slot()
     def btn_add_class_tab_handler(self) -> bool:
         if not self.lineEdit_class_name.text():
             return False
         name = self.lineEdit_class_name.text()
-        self.tabWidget.addTab(ClassTab(name), name)
+        tab = ClassTab(name)
+        self.tab_list.append(tab)
+        self.tabWidget.addTab(tab, name)
         self.lineEdit_class_name.setText("")
+        if self.tabWidget.count() > 0:
+            self.pushButton_make.setEnabled(True)
         return True
 
+    @Slot()
     def btn_delete_class_tab_handler(self) -> bool:
         if self.tabWidget.count() == 0:
             return False
-        
-        self.tabWidget.removeTab(self.tabWidget.currentIndex())
+        index = self.tabWidget.currentIndex()
+        del self.tab_list[index]
+        self.tabWidget.removeTab(index)
         return True
+    
+    def work_thread(self, classes_dir_list : list):
+        for dir_path in classes_dir_list:
+            train_names = os.listdir(dir_path)
+            print(train_names)
+        
+        pass
+
+    @Slot()
+    def btn_make_handler(self):
+        work_thread = Thread(target=self.work_thread, args=(self.tab_list,))
+        work_thread.start()
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
