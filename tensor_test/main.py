0967066a690e659c2b125f050182c74c6c3b236c
100dlswjd <100dlswjd@naver.com>
2023-08-15 19:42:36 +0900

    target은 컬럼내용에서 뺌

--- tensor_test/main.py
+++ tensor_test/main.py
@@ -1,8 +1,10 @@
 import sys
 import pandas as pd
+import copy
 
 from ui.main_form import Ui_MainWindow
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -11,6 +13,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButton_model_create.setEnabled(False)
         self.pushButton_model_create.clicked.connect(self.btn_model_create_handler)
         self.pushButton_select_csv_file_path.clicked.connect(self.btn_select_csv_file_path_handler)
+        self.comboBox.currentTextChanged.connect(self.comboBox_currentTextChanged_handler)        
+
+    def comboBox_currentTextChanged_handler(self):
+        select_column =  self.comboBox.currentText()
+        full_columns = copy.deepcopy(self.columns)
+        full_columns.remove(select_column)
+        self.label_colum_list.setText(str(full_columns))
+        pass
 
     def btn_model_create_handler(self):
         pass
@@ -24,15 +34,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.comboBox.setEnabled(True)
 
             data = pd.read_csv(file_path)
-            columns = data.columns
-            columns = list(columns)
+            self.columns = data.columns
+            self.columns = list(self.columns)
             self.comboBox.clear()
-            for idx in columns:
+            for idx in self.columns:
                 self.comboBox.addItem(idx)
-            self.label_colum_list.setText(str(columns))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    app.setStyle('Fusion')
     window.show()
     app.exec()
